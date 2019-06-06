1a414d68bd49ce7b5e760d0efa7d5e74b7806bdb
Martijn Buurman <martijn@m19.nl>
2019-06-06 23:03:37 +0800

    add function to deal cards

--- main.go
+++ main.go
@@ -32,35 +32,43 @@ func chunkArray(arr []byte, chunkSize int) [][]byte {
 	return groups
 }
 
-func bytesToFloat64(arr [][]byte) float64 {
+func bytesToFloat64(arr []byte) float64 {
 	var total float64
 
 	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr[i]); j++ {
-			value := float64(arr[i][j]) / math.Pow(256, float64(j+1))
-			total = total + value
-		}
+		value := float64(arr[i]) / math.Pow(256, float64(i+1))
+		total = total + value
 	}
 
 	return total
 }
 
-func calculateRoll(clientSeed string, serverSeed string, nonce int, round int) float64 {
-	limit := 1
+func calculateRolls(clientSeed string, serverSeed string, nonce int, round int, chunk int, limit int) []float64 {
 	bytes := hmacSha256(clientSeed, serverSeed, nonce, round)
-	bytesArray := chunkArray(bytes, 4)
-	bytesArray = bytesArray[0:limit]
+	bytesArray := chunkArray(bytes, chunk)
 
-	fmt.Println(bytes)
-	fmt.Println(bytesArray)
+	if chunk*limit > 32 {
+		panic("hash is only 32 bytes big")
+	}
 
-	total := bytesToFloat64(bytesArray)
-	return total
+	if limit < 8 {
+		bytesArray = bytesArray[0:limit]
+	}
+
+	var totals []float64
+
+	for i := 0; i < len(bytesArray); i++ {
+		totals = append(totals, bytesToFloat64(bytesArray[i]))
+	}
+
+	return totals
 }
 
 func calculateLimbo(clientSeed string, serverSeed string, nonce int) float64 {
+	chunk := 4
+	limit := 1
 	round := 0
-	total := calculateRoll(clientSeed, serverSeed, nonce, round)
+	total := calculateRolls(clientSeed, serverSeed, nonce, round, chunk, limit)[0]
 	total = total * 100000000
 
 	result := 1000000 / (math.Floor(total) + 1) * houseEdge
@@ -69,11 +77,38 @@ func calculateLimbo(clientSeed string, serverSeed string, nonce int) float64 {
 }
 
 func calculateDice(clientSeed string, serverSeed string, nonce int) float64 {
+	chunk := 4
+	limit := 1
 	round := 0
-	total := calculateRoll(clientSeed, serverSeed, nonce, round)
+	total := calculateRolls(clientSeed, serverSeed, nonce, round, chunk, limit)[0]
 	total = total * 10001
 
 	return total / 100
+}
+
+func calculateCards(clientSeed string, serverSeed string, nonce int) []float64 {
+	chunk := 4
+	limit := 8
+
+	var cardIndexes []float64
+
+	// this will get the first 6*8 = 48 cards
+	for i := 0; i < 6; i++ {
+		rolls := calculateRolls(clientSeed, serverSeed, nonce, i, chunk, limit)
+		for j := 0; j < len(rolls); j++ {
+			value := math.Floor(rolls[j] * 52)
+			cardIndexes = append(cardIndexes, value)
+		}
+	}
+
+	// we only need 4 more bytes to get to 52 cards
+	rolls := calculateRolls(clientSeed, serverSeed, nonce, 6, chunk, 4)
+	for j := 0; j < len(rolls); j++ {
+		value := math.Floor(rolls[j] * 52)
+		cardIndexes = append(cardIndexes, value)
+	}
+
+	return cardIndexes
 }
 
 func main() {
@@ -83,4 +118,5 @@ func main() {
 
 	fmt.Println("limbo:", calculateLimbo(clientSeed, serverSeed, nonce))
 	fmt.Println("dice:", calculateDice(clientSeed, serverSeed, nonce))
+	fmt.Println("cards:", calculateCards(clientSeed, serverSeed, nonce))
 }
